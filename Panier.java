d26ef1c28eeef88ef18f2d8e78ab9794408c7854
Samlach22 <44367571+samlach2222@users.noreply.github.com>
2022-09-13 16:16:42 +0200

    ajout Panier

--- Panier.java
+++ Panier.java
@@ -6,80 +6,122 @@ import java.util.*;
 public class Panier {
     private ArrayList<Fruit> fruits;  //attribut pour stocker les fruits
     private int contenanceMax;        //nb maximum d'oranges que peut contenir le panier
-	
+
     //groupe 1
     public Panier(int contenanceMax){  //initialise un panier vide ayant une certaine contenance maximale (precisee en parametre)
-	
+        this.fruits = new ArrayList<Fruit>();
+        this.contenanceMax = contenanceMax;
     }
 
     @Override
     public String toString(){  //affichage de ce qui est contenu dans le panier : liste des fruits presents
-        return "";
+        String resultat = "";
+        for (int i = 0; i < this.getFruits().size(); i++)
+        {
+            Fruit f = this.getFruit(i);
+            if(f != null)
+            {
+                resultat += f.toString() + "/n";
+            }
+        }
+        return resultat;
     }
-    
+
     //groupe 2
     public ArrayList<Fruit> getFruits() {  //accesseur du premier attribut
-       return null;
+        return this.fruits;
     }
 
     public void setFruits(ArrayList<Fruit> fruits) { //modificateur du premier attribut
-      
+        this.fruits = fruits;
     }
 
     public int getTaillePanier(){  //accesseur retournant la taille allouee pour l'attibut fruits
-        return 0;
+        return this.getFruits().size();
     }
-    
+
     public int getContenanceMax(){  //accesseur du second attribut
-	return 0;
+        return this.contenanceMax;
     }
 
     //groupe 3
     public Fruit getFruit(int i){  //accesseur retournant le fruit contenu dans le panier a l'emplacement n°i ou null s'il n'y a rien a cet emplacement
-	return null;
+	   return this.getFruits().get(i);
     }
-    
-    public void setFruit(int i, Fruit f){  //modificateur du fruit contenu dans le panier a l'emplacement n°i par f (s'il y a bien deja un fruit a cet emplacement, ne rien faire sinon)
 
+    public void setFruit(int i, Fruit f){  //modificateur du fruit contenu dans le panier a l'emplacement n°i par f (s'il y a bien deja un fruit a cet emplacement, ne rien faire sinon)
+        this.getFruits().set(i, f);
     }
-    
+
     public boolean estVide(){  //predicat indiquant que le panier est vide
-	return false;
+    	if(this.getFruits().isEmpty()) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
-    
+
     public boolean estPlein(){  //predicat indiquant que le panier est plein
-	return false;
+    	if(this.getTaillePanier() >= this.getContenanceMax()){
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
     //groupe 4
     public void ajout(Fruit o) throws PanierPleinException{  //ajoute le fruit o a la fin du panier si celui-ci n'est pas plein
-       
+        if(!this.estPlein()){
+            this.getFruits().add(o);
+        }
     }
 
     //groupe 5
     public void retrait() throws PanierVideException{ //retire le dernier fruit du panier si celui-ci n'est pas vide
-     
+        if(!this.estVide()){
+            this.getFruits().remove(this.getTaillePanier() - 1);
+        }
     }
 
     //groupe 6
     public double getPrix(){  //calcule le prix du panier par addition des prix de tous les fruits contenus dedans
-	return 0;
+        double prixTotal = 0.0;
+        for (int i = 0; i < this.getTaillePanier(); i++)
+        {
+            Fruit f = this.getFruit(i);
+            if(f != null)
+            {
+                prixTotal += f.getPrix();
+            }
+        }
+        return prixTotal;
     }
-    
+
     //groupe 7
     public void boycotteOrigine(String origine){  //supprime du panier tous les fruits provenant du pays origine
-	
-    }  
-        
-    //groupe 8    
+        for (int i = 0; i < this.getTaillePanier(); i++)
+        {
+            Fruit f = this.getFruit(i);
+            if(f != null)
+            {
+                if(f.getOrigine() == origine){
+                    this.getFruits().remove(i);
+                }
+            }
+        }
+    }
+
+    //groupe 8
     @Override
     public boolean equals(Object o){  ///predicat pour tester si 2 paniers sont equivalents : s'ils contiennent exactement les memes fruits
         return false;
     }
-    
+
     //tests
     public static void main (String[] args){
     	//Ecrire ici vos tests
-	System.out.println("premier test Panier");
+	    System.out.println("premier test Panier");
     }
 }
